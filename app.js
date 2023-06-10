207b4e5e35363be028ec09ac7874077270bd9020
Henrique Silva <henrique@henriquesilva.dev>
2023-06-10 11:32:31 -0300

    feat: create main function with menu options

---
+++ app.js
@@ -0,0 +1,12 @@
+
+const main = () => {
+  const menu = "\tAgendamento de Consultas Clinicas\n\n" +
+    "\t1. Cadastrar paciente\n" +
+    "\t2. Marcar consulta\n" +
+    "\t3. Cancelar consulta\n" +
+    "\t4. Sair\n" +
+    "\t----------------------------------\n";
+  console.log(menu);
+}
+
+main();
